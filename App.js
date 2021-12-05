dd1216b50bf4f36c30f7cb60873ff5de93be200a
Rohit Patil <71687498+falconcode16@users.noreply.github.com>
2021-12-05 22:19:29 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,27 @@
+
+import './App.css';
+import React from 'react'
+import Navbar from './Components/Navbar';
+import TextForm from './Components/TextForm';
+
+
+
+function App() {
+  return (
+  <>
+   
+<Navbar title="Textvisuals"/>
+<div className="container">
+
+<TextForm heading="Enter the text to analyze below"/>
+
+</div>
+
+
+
+
+  </>
+  );
+}
+
+export default App;
